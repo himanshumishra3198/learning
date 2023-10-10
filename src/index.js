8456bbc6a68d018894be1aaaa5285c8f63218e6f
Himanshu Mishra <64432421+himanshumishra3198@users.noreply.github.com>
2023-10-10 21:17:05 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+var name = "Himanshu";
+var date = new Date();
+var n = date.getFullYear();
+// var date = date.getYear();
 ReactDOM.render(
   <div>
-    <h1>Hello world,</h1>
-    <p>get a job</p>
+    <p>Hello {name} </p> <p> copyright {n}</p>{" "}
   </div>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
