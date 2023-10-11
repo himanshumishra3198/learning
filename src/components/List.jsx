0dc8850232e1a7e2ea6e6bcceb46ee1d5261b7f3
Himanshu Mishra <64432421+himanshumishra3198@users.noreply.github.com>
2023-10-11 22:33:32 +0000

    Update 5 files

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Bacon</li>
+      <li> Samosa</li>
+      <li> Rasogulla</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const date = new Date();
-const time = date.getHours();
-var period;
-var theme;
-if (0 <= time && time <= 12) {
-  period = "morning";
-  theme = { color: "red" };
-} else if (12 < time && time <= 18) {
-  period = "afternoon";
-  theme = { color: "green" };
-} else {
-  period = "evening";
-  theme = { color: "blue" };
-}
-ReactDOM.render(
-  <div>
-    <h1 style={theme}>Good {period}</h1>
-  </div>,
-  document.getElementById("root")
-);
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
